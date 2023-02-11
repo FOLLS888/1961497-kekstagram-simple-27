691f2708d724c7cbce4f18af806a36e14502ba7c
folls888 <94069812+FOLLS888@users.noreply.github.com>
2023-02-11 13:05:46 +0300

    25 сгенерированных объекта

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-function getRandomNumber(min, max) {
-  if (typeof min === 'number' && typeof max === 'number') {
-    return Math.round(Math.random() * (max - min)) + min; //Генерируем float от 0 до 1 и приводим его к заданным границам
-  }
-  return NaN;
-}
-getRandomNumber(1, 132);
+const getRandomNumber = (min, max) => {
 
-function checkValidComment(targetString, maxLength) {
-  if (typeof targetString === 'string') {
-    return !(targetString.length > maxLength);
+  if (min < 0 || min > max) {
+    return NaN;
   }
-  return 'В функцию передана не строка!';
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  // использован материал https://habr.com/ru/post/312880/
+};
+
+const checkLengthString = (checkString, maxLength) => checkString.length < maxLength;
+
+
+checkLengthString('какой то текст ', 50);
+
+const testSizeArray = 25;
+const likeGenMin = 15;
+const likeGenMax = 200;
+const commentsGenMax = 200;
+const testObjects = [];
+
+for (let i = 0; i < testSizeArray;) {
+  i += 1;
+  testObjects.push({
+    id: i,
+    url: `photos/${i}.jpg`,
+    description: 'Описание фото номер',
+    likes: getRandomNumber(likeGenMin, likeGenMax),
+    comments: getRandomNumber(0, commentsGenMax),
+  }
+  );
 }
-checkValidComment(23, 1);
